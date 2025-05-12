3ba08b4708fc6a012395ccf8c71328fd824dc752
Nguyen Minh Luan <luannguyen0515@gmail.com>
2025-05-12 14:29:24 +0700

    Sort middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -16,26 +18,51 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
 app.use(methodOverride('_method'));
+
+// Custom Middleware
+app.use(SortMiddleware);
 
 // HTTP logger
 // app.use(morgan("combined"));
 
 // Template engine
 app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-        helpers: {
-            sum: (a, b) => a + b,
-        },
-    }),
+  'hbs',
+  engine({
+    extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default';
+
+        const icons = {
+          default: 'fa-solid fa-sort',
+          asc: 'fa-solid fa-arrow-down-wide-short',
+          desc: 'fa-solid fa-arrow-down-short-wide',
+        };
+
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc',
+        };
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+            <i class="${icon}"></i>
+          </a>`;
+      },
+    },
+  })
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -44,5 +71,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
