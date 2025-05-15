eb3bc828e012d3d9682d6525ddf40a7746a6aa1e
Nguyen Minh Luan <75821049+l2002@users.noreply.github.com>
2025-05-15 12:58:37 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,8 +70,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Routes init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
 
 module.exports = app;
